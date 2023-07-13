eff37c0c68249e2f59544e1879a3edb39c30cbb8
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-07-13 19:59:01 -0300

    hola cargil con footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react"
-
-import "./App.css"
-import "./App.css"
 import Navbar1 from "./components/navbar1.jsx"
 import Navbar from "./components/navbar2.jsx"
 import Footer from "./components/footer.jsx"
+
 const App = () => {
   return (
     <>
-      <div>
-        <Navbar1 />
-        <Navbar />
+      <div style={{ minHeight: "200vh" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            position: "absolute",
+            width: "100%",
+          }}
+        >
+          <Navbar1 />
+          <Navbar />
+        </div>
         <Footer />
         <h1>React App</h1>
       </div>
--- src/components/navbar2.jsx
+++ src/components/navbar2.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react"
 import "./navbar2.css"
+import { useState } from "react"
+
 const Navbar = () => {
+  const [scroll, setScroll] = useState(false)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 90 ? true : false)
+    })
+  })
   return (
-    <nav className="navbar2">
+    <nav className={`navbar2 ${scroll && "navbar-2-scrolled"}`}>
       <div className="navbar2-content">
         <ul className="navbar2-ul">
           <li className="navbar2-li">Home</li>
